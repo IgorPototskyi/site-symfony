f37ebf924960bce33c33deac42ab9a6736c4d780
IgorPototskyi <igorekpototsky@gmail.com>
2017-05-30 16:37:48 +0300

    add about section

--- app/js/common.js
+++ app/js/common.js
@@ -17,4 +17,58 @@ $(function() {
 		$('.hamburger').removeClass('is-active');
 	});
 
+	$('.carousel-services').on('initialized.owl.carousel', function() {
+		setTimeout(function() {
+			carouselService()
+		}, 100)
+	});
+
+	$('.carousel-services').owlCarousel({
+		loop: true,
+		nav: true,
+		dots: false,
+		smartSpeed: 700,
+		navText: ['<i class="fa fa-angle-double-left"></i>', '<i class="fa fa-angle-double-right"></i>'],
+		responsiveClass: true,
+		responsive: {
+			0: {
+				items: 1
+			},
+			800: {
+				items: 2
+			},
+			1100: {
+				items: 3
+			},
+		},
+	});
+
+	function carouselService() {
+		$('.carousel-services-item').each(function() {
+			var ths = $(this),
+				thsHeight = ths.find('.carousel-services-item-content').outerHeight();
+			
+			ths.find('.carousel-services-item-image').css('min-height', thsHeight);
+		});
+	}
+
+	$('.carousel-services-item-composition .h3').each(function() {
+		var ths = $(this);
+		ths.html(ths.html().replace(/(\S+)\s*$/, '<span>$1</span>'));
+	});
+	$('section .h2').each(function() {
+		var ths = $(this);
+		ths.html(ths.html().replace(/^(\S+)/, '<span>$1</span>'));
+	});
+
+	// Resize Window
+	function onResize() {
+		$('.carousel-services-item-content').equalHeights();
+	}
+	onResize();
+	window.onresize = function() {
+		onResize();
+		carouselService();
+	};
+
 });
--- gulpfile.js
+++ gulpfile.js
@@ -28,6 +28,9 @@ gulp.task('js', ['common-js'], function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
 		'app/libs/jQuery.mmenu/dist/jquery.mmenu.all.js',
+		'app/libs/owl.carousel/owl.carousel.min.js',
+		'app/libs/equalheights/equalheights.js',
+		'app/libs/fotorama/fotorama.js',
 		'app/js/common.min.js', // Всегда в конце
 		])
 	.pipe(concat('scripts.min.js'))
