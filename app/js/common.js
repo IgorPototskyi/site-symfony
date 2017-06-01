37e4a30daa405cc45441346aa80bbbe64e5eed57
IgorPototskyi <igorekpototsky@gmail.com>
2017-06-01 17:29:06 +0300

    add footer

--- app/js/common.js
+++ app/js/common.js
@@ -63,6 +63,43 @@ $(function() {
 
 	// $('select').selectize();
 
+	$('.carousel-reviews').owlCarousel({
+		loop: true,
+		nav: false,
+		items: 1,
+		dots: true,
+		smartSpeed: 700,
+		// autoHeight: true,
+		autoplay: true,
+		autoplayTimeout: 10000,
+	});
+
+	$('.carousel-partners').owlCarousel({
+		loop: true,
+		smartSpeed: 700,
+		dots: false,
+		nav: true,
+		navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+		responsiveClass: true,
+		responsive: {
+			0: {
+				items: 1
+			},
+			480: {
+				items: 2
+			},
+			768: {
+				items: 3
+			},
+			992: {
+				items: 3
+			},
+			1200: {
+				items: 4
+			},
+		},
+	});
+
 	//E-mail Ajax Send
 	$("form.callback").submit(function() { //Change
 		var th = $(this);
